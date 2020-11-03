713bbd855c293cfda1f830272f341cbdb70bd5ab
Qingtian239113 <980287353@qq.com>
2020-11-03 22:15:37 +0800

    打包1.0.4

--- src/main/java/com/kepai/base/annotation/EnableKepaiBase.java
+++ src/main/java/com/kepai/base/annotation/EnableKepaiBase.java
@@ -13,5 +13,5 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE})
 @Documented
 @Import({KepaiCommonConfiguration.class})
-public @interface EnableKepaiCommon {
+public @interface EnableKepaiBase {
 }
